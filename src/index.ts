fc31c8c5edb85a843b8e3ab33f444a270e2d9b9e
Shakhawat <shakhawat2021hossain2021@gmail.com>
2025-05-08 20:40:46 +0600

    solved 2nd one

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 function formatString(input: string, toUpper?: boolean): string {
-    if(toUpper === undefined || toUpper){
+    if (toUpper === undefined || toUpper) {
         return input.toUpperCase()
     }
-    else{
+    else {
         return input.toLowerCase()
     }
+}
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    const filtered = items.filter(item => item.rating >= 4);
+    return filtered
 }
 
